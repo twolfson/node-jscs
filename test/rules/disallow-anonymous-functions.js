56f392c79c646905b0a92d59fc3dc29a41d95cea
Todd Wolfson <todd@twolfson.com>
2014-12-16 22:00:36 -0600

    Added exception test case for new behavior

--- test/rules/disallow-anonymous-functions.js
+++ test/rules/disallow-anonymous-functions.js
@@ -34,4 +34,36 @@ describe('rules/disallow-anonymous-functions', function() {
             assert(checker.checkString('var x = function named(){};').isEmpty());
         });
     });
+
+    describe('option value parameterExcept', function() {
+        beforeEach(function() {
+            checker.configure({
+                disallowAnonymousFunctions: {
+                    parameterExcept: ['it']
+                }
+            });
+        });
+
+        it('should report on anonymous function declarations', function() {
+            assert(checker.checkString('before(function(){});').getErrorCount() === 1);
+        });
+
+        it('should report on anonymous function expressions', function() {
+            assert(checker.checkString('var x = function(){};').getErrorCount() === 1);
+            assert(checker.checkString('var foo = {bar: function() {}};').getErrorCount() === 1);
+        });
+
+        it('should not report on excepted anonymous function declarations', function() {
+            assert(checker.checkString('it(\'is an exception\', function(){});').isEmpty());
+        });
+
+        it('should not report on named function declarations', function() {
+            assert(checker.checkString('function named(){};').isEmpty());
+        });
+
+        it('should not report on named function expressions', function() {
+            assert(checker.checkString('$("hi").click(function named(){});').isEmpty());
+            assert(checker.checkString('var x = function named(){};').isEmpty());
+        });
+    });
 });
