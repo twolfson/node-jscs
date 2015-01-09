55701b1acdd4fc6d608173d1d1171579c8ae4d1b
Todd Wolfson <todd@twolfson.com>
2015-01-09 01:03:55 -0600

    Added test for disallow named inline functions

---
+++ test/rules/disallow-named-inline-functions.js
@@ -0,0 +1,38 @@
+var Checker = require('../../lib/checker');
+var assert = require('assert');
+
+describe('rules/disallow-named-inline-functions', function() {
+    var checker;
+
+    beforeEach(function() {
+        checker = new Checker();
+        checker.registerDefaultRules();
+    });
+
+    describe('option value true', function() {
+        beforeEach(function() {
+            checker.configure({
+                requireNamedInlineFunctions: true
+            });
+        });
+
+        it('should not report on unnamed inline function declarations', function() {
+            assert(checker.checkString('$("hi").click(function(){});').isEmpty());
+        });
+
+        it('should not report named inline function expressions by left-hand assignment', function() {
+            assert(checker.checkString('var x = function(){};').isEmpty());
+            assert(checker.checkString('var foo = {bar: function() {}};').isEmpty());
+            assert(checker.checkString('foo.bar = function() {};').isEmpty());
+        });
+
+        it('should not report on named function declarations', function() {
+            assert(checker.checkString('function named(){};').isEmpty());
+        });
+
+        it('should report on named function expressions', function() {
+            assert(checker.checkString('$("hi").click(function named(){});').getErrorCount() === 1);
+            assert(checker.checkString('var x = function named(){};').getErrorCount() === 1);
+        });
+    });
+});
