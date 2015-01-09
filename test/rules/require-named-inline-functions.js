38e73ca7af217bb9b6c4c6017c9495dfc0544e02
Todd Wolfson <todd@twolfson.com>
2015-01-08 23:45:31 -0600

    Added new test cases

--- test/rules/require-named-inline-functions.js
+++ test/rules/require-named-inline-functions.js
@@ -35,4 +35,39 @@ describe('rules/require-named-inline-functions', function() {
             assert(checker.checkString('var x = function named(){};').isEmpty());
         });
     });
+
+    describe('option value true', function() {
+        beforeEach(function() {
+            checker.configure({
+                requireNamedInlineFunctions: {
+                    allExcept: ['it', 'it.skip']
+                }
+            });
+        });
+
+        it('should report on unnamed inline function declarations', function() {
+            assert(checker.checkString('$("hi").click(function(){});').getErrorCount() === 1);
+        });
+
+        it('should not report named inline function expressions by left-hand assignment', function() {
+            assert(checker.checkString('var x = function(){};').isEmpty());
+            assert(checker.checkString('var foo = {bar: function() {}};').isEmpty());
+            assert(checker.checkString('foo.bar = function() {};').isEmpty());
+        });
+
+        it('should not report on named function declarations', function() {
+            assert(checker.checkString('function named(){};').isEmpty());
+        });
+
+        it('should not report on named function expressions', function() {
+            assert(checker.checkString('$("hi").click(function named(){});').isEmpty());
+            assert(checker.checkString('var x = function named(){};').isEmpty());
+        });
+
+        // TODO: How to handle `forEach`?
+        it('should not report on excepted unnamed function expressions', function() {
+            assert(checker.checkString('it(function (){});').isEmpty());
+            assert(checker.checkString('it.skip(function () {});').isEmpty());
+        });
+    });
 });
